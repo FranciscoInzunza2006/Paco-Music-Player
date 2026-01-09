bfb4e85554e69dee95a99da81895057e24865c1d
Francisco Javier <FranciscoInzunza2006@gmail.com>
2026-01-08 20:05:42 -0700

    Documentation.

--- src/filesystem/filesystem.h
+++ src/filesystem/filesystem.h
@@ -11,27 +11,34 @@
 
 #ifdef __cplusplus
 extern "C" {
+
 #endif
 
-    constexpr size_t FILESYSTEM_MAX_PATH_LENGTH = 260;
+constexpr size_t FILESYSTEM_MAX_PATH_LENGTH = 260;
 
-    typedef struct {
-        wchar_t* name;
-        bool is_directory;
-    } FilesystemEntry;
+typedef struct {
+    wchar_t *name;
+    bool is_directory;
+} FilesystemEntry;
 
-    typedef struct {
-        wchar_t* path;
-        FilesystemEntry* entries;
-        size_t count;
-        size_t capacity;
-    } FilesystemDirectoryContents;
+typedef struct {
+    wchar_t *path;
+    FilesystemEntry *entries;
+    size_t count;
+    size_t capacity;
+} FilesystemDirectoryContents;
 
+    /// Takes a path to a file or directory and returns if it was found/exists.
 bool filesystemExists(const wchar_t *path);
-FilesystemDirectoryContents* filesystemDirectoryGetContents(const wchar_t *directory_path);
-    void FilesystemFreeDirectoryContents(FilesystemDirectoryContents* contents);
 
-// Returns pointer, your problem now.
+/// @param directory_path Valid directory path
+/// @return A pointer to a data structure with the contents of the directory. Free using the respective function.
+FilesystemDirectoryContents *filesystemDirectoryGetContents(const wchar_t *directory_path);
+
+    /// Releases all the memory of the FilesystemDirectoryContents structure pointed to. This functions takes also releases all the entries.
+void FilesystemFreeDirectoryContents(FilesystemDirectoryContents *contents);
+
+/// Converts a ASCII array to a wide character array. Caller gets the ownership.
 wchar_t *toWideChar(const char *str);
 
 #ifdef __cplusplus
