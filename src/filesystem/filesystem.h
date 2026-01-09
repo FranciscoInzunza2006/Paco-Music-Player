0d0323097cf3e8e6073b250a1da9c796cfadecf1
Francisco Javier <FranciscoInzunza2006@gmail.com>
2026-01-09 10:12:37 -0700

    Cleaned up that mess

--- src/filesystem/filesystem.h
+++ src/filesystem/filesystem.h
@@ -7,18 +7,9 @@
 #include <stddef.h>
 #include <windows.h>
 
-#ifdef _WIN32
-  #ifdef FILESYSTEM_EXPORTS
-    #define FS_API __declspec(dllexport)
-  #else
-    #define FS_API __declspec(dllimport)
-  #endif
-#else
-  #define FS_API
-#endif
-
 #ifdef __cplusplus
 extern "C" {
+
 
 #endif
 
@@ -36,18 +27,18 @@ typedef struct {
     size_t capacity;
 } FilesystemDirectoryContents;
 
-    /// Takes a path to a file or directory and returns if it was found/exists.
-FS_API bool filesystemExists(const wchar_t *path);
+/// Takes a path to a file or directory and returns if it was found/exists.
+bool filesystemExists(const wchar_t *path);
 
 /// @param directory_path Valid directory path
 /// @return A pointer to a data structure with the contents of the directory. Free using the respective function.
-FS_API FilesystemDirectoryContents *filesystemDirectoryGetContents(const wchar_t *directory_path);
+FilesystemDirectoryContents *filesystemDirectoryGetContents(const wchar_t *directory_path);
 
-    /// Releases all the memory of the FilesystemDirectoryContents structure pointed to. This functions takes also releases all the entries.
-FS_API void filesystemFreeDirectoryContents(FilesystemDirectoryContents *contents);
+/// Releases all the memory of the FilesystemDirectoryContents structure pointed to. This functions takes also releases all the entries.
+void filesystemFreeDirectoryContents(FilesystemDirectoryContents *contents);
 
 /// Converts a ASCII array to a wide character array. Caller gets the ownership.
-FS_API wchar_t *toWideChar(const char *str);
+wchar_t *toWideChar(const char *str);
 
 #ifdef __cplusplus
 }
