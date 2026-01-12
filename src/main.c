f97049e3135cfbef86236ac4c98dc841794962a1
Francisco Javier <FranciscoInzunza2006@gmail.com>
2026-01-11 19:43:05 -0700

    Removed custom filesystem library implementation

--- src/main.c
+++ src/main.c
@@ -1,7 +1,5 @@
-//#include "raylib.h"
-#include <stdio.h>
 
-#include "filesystem/filesystem.h"
+#include <raylib.h>
 
 int screenWidth = 800;
 int screenHeight = 450;
@@ -9,36 +7,9 @@ int screenHeight = 450;
 void UpdateDrawFrame(void); // Update and Draw one frame
 
 int main() {
-    DirectoryList* dl;
-    getDirectoryList("C:\\Users\\Francisco Javier\\foo.txt", &dl);
 
-    for (size_t i = 0; i < dl->count; i++) {
-        printf("%s\n", dl->entries[i].name);
-    }
-
-    freeDirectoryList(dl);
-
-    return 0;
-
-    // InitWindow(screenWidth, screenHeight, "Paco's Music Player");
-    // SetTargetFPS(24);
-    // while (!WindowShouldClose())
-    // {
-    //     UpdateDrawFrame();
-    // }
-    //
-    // CloseWindow();
-    // return 0;
 }
 
 void UpdateDrawFrame(void) {
-    // // Update
-    //
-    // // Draw
-    // BeginDrawing();
-    // ClearBackground(BLACK);
-    //
-    // DrawText("Your mom", 190, 200, 20, RAYWHITE);
-    //
-    // EndDrawing();
+
 }
--- tests/test_example/example_tests.cpp
+++ tests/test_example/example_tests.cpp
@@ -2,9 +2,8 @@
 // Created by Francisco Javier on 29/12/2025.
 //
 
-#include "filesystem.h"
 #include <gtest/gtest.h>
 
-TEST(FilesystemTest, yes) {
+TEST(example, example) {
     ASSERT_TRUE(true);
 }
